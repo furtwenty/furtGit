7432c0a5e2f275175fb97157b155d5f0bbf242ad
Furtwenty <furtwenty2@gmail.com>
2016-07-28 02:10:37 -0500

    updated bad post calls
    i removed an unnecessary post call in viewPost.php added function call
    to update post from within viewpost.php.

--- blog/viewPosts.php
+++ blog/viewPosts.php
@@ -67,10 +67,7 @@ function loadScripts(){
 				$.post("viewPosts.php", 
 				{action:one,subject:sub,blog:blo,title:tit,bindx:ind},
 				function(data){
-	 				$.post( "updatePosts.php",
-	 				{up:"one"},	
-					function(data){
-						
+	 			
 						$.post( "viewPosts.php",
 	 					{action:"two",subject:sub,blog:blo,title:tit,bindx:ind},	
 						function(data){
@@ -82,8 +79,6 @@ function loadScripts(){
 			
 						}
 						);
-					}
-					);
 				
 				$("#dlog").hide();
 				});
@@ -107,11 +102,6 @@ function loadScripts(){
 			});
 	
 			
-			
-			
-			
-			
-			
 			});
 			</script>
 			
@@ -126,6 +116,9 @@ function setBlogSession(){
 	$_SESSION['btit'] = $_POST['title'];
 	$_SESSION['bsub'] = $_POST['subject'];
 	$_SESSION['blog'] = $_POST['blog'];
+	
+	updatePost();
+	
 	
 }
 
@@ -226,5 +219,98 @@ function displayTable()
 
 }
 
+//echo var_dump($_SESSION);
+//echo var_dump($_GET);
+//echo var_dump($_POST);
+function updatePost(){
+
+	date_default_timezone_set('America/Mexico_City');
+
+	$fileParse = file_get_contents(".\\post.txt");
+
+	$j = json_decode($fileParse,true);
+
+	$setFlag=0;
+	$i = 0;
+
+	//holds value of consturcted blog list
+	$p = array();
+
+	if($_SESSION['indx']!=""){
+		foreach ($j['postList'] as &$pobject){
+
+			if($_SESSION['indx']==$i){
+
+				$pobject['data']['title'] = $_SESSION['btit'];
+				$pobject['data']['subject'] = $_SESSION['bsub'];
+				$pobject['data']['postData'] = $_SESSION['blog'];
+
+
+
+				$setFlag=1;
+			}
+			$i++;
+		}
+		$p = $j;
+	}
+	if($setFlag==0){
+
+
+		$username = $_SESSION['name'];
+
+		$title = $_SESSION['btit'];
+		$subject = $_SESSION['bsub'];
+		$postData = $_SESSION['blog'];
+
+		$data = array("title"=>$title,"subject"=>$subject,"postData"=>$postData);
+
+
+
+		$month = date('m');
+		$day = date('d');
+		$year = date('Y');
+
+		$date = array("month"=>$month,"day"=>$day,"year"=>$year);
+
+		$hour = date('g');
+		$min = date('i');
+		$sec = date('s');
+		$timee = time();
+
+		$time = array("hour"=>$hour,"min"=>$min,"sec"=>$sec,"elsp"=>$timee);
+
+		$timestamp = array("date"=>$date,"time"=>$time);
+
+		$post = array("usrname"=>$username,"data"=>$data,"timestamp"=>$timestamp);
+
+
+		$a = array();
+		array_push($a,$post);
+
+		foreach($j['postList'] as $object){
+			array_push($a,$object);
+		}
+
+		$b = ["postList"=>$a];
+		$p = $b;
+	}
+
+	//print_r(json_encode($j));
+
+	//print_r("\n");
+	//$b = array_reverse($j['postList']);
+
+	//$c = ["postList"=>$b];
+	//print_r(json_encode($c));
+
+	$d = json_encode($p);
+
+	//print_r($j);
+
+	file_put_contents(".\\post.txt",$d);
+
+	$_SESSION['indx'] = "";
+
+}
 
 ?>
